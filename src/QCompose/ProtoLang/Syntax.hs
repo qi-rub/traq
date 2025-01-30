db6f7c9e1980490ab44deb5972db7f35df9cd012
Anurudh Peduri <anurudhp97@gmail.com>
2025-01-30 12:49:21 +0100

    WIP unitary QPL

--- src/QCompose/ProtoLang/Syntax.hs
+++ src/QCompose/ProtoLang/Syntax.hs
@@ -1,6 +1,7 @@
 module QCompose.ProtoLang.Syntax where
 
 import qualified Data.Map as M
+import qualified Data.Set as S
 import QCompose.Basic
 
 -- proto-search language
@@ -30,3 +31,21 @@ data FunDef = FunDef [(Ident, VarType)] [(Ident, VarType)] Stmt -- args@(x_i : T
   deriving (Eq, Show, Read)
 
 type FunCtx = M.Map Ident FunDef
+
+-- syntactic properties
+stmtVars :: Stmt -> S.Set Ident
+stmtVars (SAssign x x') = S.fromList [x, x']
+stmtVars (SConst x _ _) = S.fromList [x]
+stmtVars (SUnOp x _ y) = S.fromList [x, y]
+stmtVars (SBinOp x _ lhs rhs) = S.fromList [x, lhs, rhs]
+stmtVars (SOracle xs ys) = S.fromList $ xs <> ys
+stmtVars (SFunCall xs _ ys) = S.fromList $ xs <> ys
+stmtVars (SIfTE b s_t s_f) =
+  let vars = S.union (stmtVars s_t) (stmtVars s_f)
+   in S.insert b vars
+stmtVars (SSeq s_1 s_2) = S.union (stmtVars s_1) (stmtVars s_2)
+stmtVars (SSearch x ok _ args) = S.fromList (x : ok : args)
+stmtVars (SContains ok _ args) = S.fromList (ok : args)
+
+funVars :: FunDef -> S.Set Ident
+funVars (FunDef params rets body) = S.union (S.fromList (map fst params <> map fst rets)) (stmtVars body)
--- src/QCompose/UnitaryQPL/Lowering.hs
+++ src/QCompose/UnitaryQPL/Lowering.hs
@@ -5,3 +5,17 @@ import QCompose.ProtoLang.Syntax
 import QCompose.UnitaryQPL.Syntax
 
 lowerU :: Precision -> Stmt -> UQPLStmt
+lowerU _ (SAssign x x') = UUnitary [x', x] "XorInto"
+lowerU _ (SConst x v ty) = UUnitary [x] ("XorConst" <> show v)
+lowerU _ (SUnOp res PNot arg) = UUnitary [arg, res] "C[0]-X"
+lowerU _ (SBinOp res PAdd lhs rhs) = UUnitary [lhs, rhs, res] "AddInto"
+lowerU _ (SBinOp res PLeq lhs rhs) = UUnitary [lhs, rhs, res] "LEqInto"
+lowerU _ (SBinOp res PAnd lhs rhs) = UUnitary [lhs, rhs, res] "Toffoli"
+lowerU _ (SOracle res args) = UOracle (args <> res)
+-- lowerU delta (SFunCall res fname args) = UOracle (args <> res)
+lowerU delta (SSeq s1 s2) = USeq (lowerU (delta / 2) s1) (lowerU (delta / 2) s2)
+lowerU delta (SContains ok fname args) = undefined
+lowerU _ _ = error "unsupported syntax"
+
+lowerUDef :: Precision -> FunDef -> UProcDef
+lowerUDef delta fn = _
