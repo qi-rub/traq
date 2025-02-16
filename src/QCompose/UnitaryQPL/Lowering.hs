7fdf258b66bfd07d6c21bac160c6efb1c1cf544e
Anurudh Peduri <anurudhp97@gmail.com>
2025-02-16 19:21:41 +0100

    fix all warnings

--- src/QCompose/UnitaryQPL/Lowering.hs
+++ src/QCompose/UnitaryQPL/Lowering.hs
@@ -5,17 +5,18 @@ import QCompose.ProtoLang.Syntax
 import QCompose.UnitaryQPL.Syntax
 
 lowerU :: Precision -> Stmt -> UQPLStmt
-lowerU _ (SAssign x x') = UUnitary [x', x] XorInto
-lowerU _ (SConst x v _) = UUnitary [x] (XorConst v)
-lowerU _ (SUnOp res PNot arg) = UUnitary [arg, res] C0_X
-lowerU _ (SBinOp res PAdd lhs rhs) = UUnitary [lhs, rhs, res] AddInto
-lowerU _ (SBinOp res PLeq lhs rhs) = UUnitary [lhs, rhs, res] LEqInto
-lowerU _ (SBinOp res PAnd lhs rhs) = UUnitary [lhs, rhs, res] Toffoli
-lowerU _ (SOracle res args) = UOracle (args <> res)
--- lowerU delta (SFunCall res fname args) = UOracle (args <> res)
+lowerU _ SAssign{..} = UUnitary [arg, ret] XorInto
+lowerU _ SConst{..} = UUnitary [ret] (XorConst val)
+lowerU _ SUnOp{..} = UUnitary [arg, ret] C0_X
+lowerU _ SBinOp{..} = UUnitary [lhs, rhs, ret] opUnitary
+  where
+    opUnitary = case bin_op of
+      PAdd -> AddInto
+      PLeq -> LEqInto
+      PAnd -> Toffoli
+lowerU _ SOracle{..} = UOracle (args <> rets)
 lowerU delta (SSeq s1 s2) = USeq (lowerU (delta / 2) s1) (lowerU (delta / 2) s2)
-lowerU delta (SContains ok fname args) = undefined
 lowerU _ _ = error "unsupported syntax"
 
 lowerUDef :: Precision -> FunDef -> UProcDef
-lowerUDef delta fn = error "not implemented"
+lowerUDef _ _ = error "not implemented"
