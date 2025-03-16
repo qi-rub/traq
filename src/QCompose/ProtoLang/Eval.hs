866de429dbf461ad0f1c45185bf1b5b0567afb69
Anurudh Peduri <anurudhp97@gmail.com>
2025-03-17 00:43:41 +0100

    cleanup

--- src/QCompose/ProtoLang/Eval.hs
+++ src/QCompose/ProtoLang/Eval.hs
@@ -7,11 +7,13 @@ module QCompose.ProtoLang.Eval (
   evalProgram,
 ) where
 
-import Control.Monad.Extra (anyM, filterM)
+import Control.Monad (filterM, zipWithM_)
+import Control.Monad.Extra (anyM)
 import Control.Monad.State (StateT, evalStateT, get)
 import Control.Monad.Trans (lift)
 import qualified Data.Map as M
 import Lens.Micro
+import Lens.Micro.Mtl
 
 import QCompose.Basic
 import QCompose.ProtoLang.Syntax
@@ -44,68 +46,62 @@ evalBinOp AndOp 0 _ = 0
 evalBinOp AndOp _ 0 = 0
 evalBinOp AndOp _ _ = 1
 
-evalStmt :: FunCtx SizeT -> OracleInterp -> Stmt SizeT -> Evaluator [(Ident, Value)]
+evalStmt :: FunCtx SizeT -> OracleInterp -> Stmt SizeT -> Evaluator ()
 -- basic statements
 evalStmt _ _ AssignS{ret, arg} = do
-  arg_val <- lookupVar arg
-  return [(ret, arg_val)]
+  at ret <~ use (at arg)
 evalStmt _ _ ConstS{ret, val} = do
-  return [(ret, val)]
+  at ret ?= val
 evalStmt _ _ UnOpS{ret, un_op, arg} = do
   arg_val <- lookupVar arg
-  let ret_val = evalUnOp un_op arg_val
-  return [(ret, ret_val)]
+  at ret ?= evalUnOp un_op arg_val
 evalStmt _ _ BinOpS{ret, bin_op, lhs, rhs} = do
   lhs_val <- lookupVar lhs
   rhs_val <- lookupVar rhs
-  let ret_val = evalBinOp bin_op lhs_val rhs_val
-  return [(ret, ret_val)]
+  at ret ?= evalBinOp bin_op lhs_val rhs_val
 
 -- compound statements
 evalStmt funCtx oracleF IfThenElseS{cond, s_true, s_false} = do
   cond_val <- lookupVar cond
   let s = if cond_val == 0 then s_false else s_true
   evalStmt funCtx oracleF s
-evalStmt funCtx oracleF (SeqS ss) = do
-  mapM_ (\s -> evalProgram Program{funCtx, stmt = s} oracleF) ss
-  return []
-
+evalStmt funCtx oracleF (SeqS ss) = mapM_ (evalStmt funCtx oracleF) ss
 -- function calls
 evalStmt _ oracleF FunCallS{fun_kind = OracleCall, args, rets} = do
   arg_vals <- mapM lookupVar args
   let ret_vals = oracleF arg_vals
-  return $ zip rets ret_vals
+  zipWithM_ putValue rets ret_vals
 evalStmt funCtx oracleF FunCallS{fun_kind = FunctionCall fun, args, rets} = do
   arg_vals <- mapM lookupVar args
   fun_def <- lift $ funCtx & lookupFun fun
   ret_vals <- lift $ evalFun funCtx oracleF arg_vals fun_def
-  return $ zip rets ret_vals
+  zipWithM_ putValue rets ret_vals
 
 -- subroutines
 -- `any` / `search`
 evalStmt funCtx oracleF FunCallS{fun_kind = SubroutineCall sub, rets = (ok : rets), args = (predicate : args)}
   | sub == Contains = do
-      s_arg_ty <- getSearchArgTy
+      vals <- getSearchRange
       sigma <- get
+      has_sol <- lift $ anyM (evalPredicate sigma) vals
 
-      has_sol <- lift $ anyM (evalPredicate sigma) (range s_arg_ty)
-      return [(ok, boolToValue has_sol)]
+      at ok ?= boolToValue has_sol
   | sub == Search = do
-      s_arg_ty <- getSearchArgTy
-      sigma <- get
-
       let [sol] = rets
-      let vals = range s_arg_ty
 
+      vals <- getSearchRange
+      sigma <- get
       sols <- lift $ filterM (evalPredicate sigma) vals
+
       let has_sol = not $ null sols
       let out_vals = if has_sol then sols else vals
-      lift $ choice [pure [(ok, boolToValue has_sol), (sol, v)] | v <- out_vals]
+      binds <- lift $ choice [pure [(ok, boolToValue has_sol), (sol, v)] | v <- out_vals]
+      mapM_ (uncurry putValue) binds
   where
-    getSearchArgTy = do
+    getSearchRange = do
       FunDef{param_binds = pred_param_binds} <- lift $ funCtx & lookupFun predicate
       let (_, s_arg_ty) = last pred_param_binds
-      return s_arg_ty
+      return $ range s_arg_ty
 
     evalPredicate :: ProgramState -> Value -> Tree Bool
     evalPredicate sigma val = evalStateT (runPredicate "_search_arg" val) sigma
@@ -123,17 +119,15 @@ evalStmt funCtx oracleF FunCallS{fun_kind = SubroutineCall sub, rets = (ok : ret
 -- fallback
 evalStmt _ _ _ = error "unsupported"
 
-evalProgram :: Program SizeT -> OracleInterp -> Evaluator [(Ident, Value)]
-evalProgram Program{funCtx, stmt} oracleF = do
-  rets <- evalStmt funCtx oracleF stmt
-  mapM_ (uncurry putValue) rets
-  return rets
-
 evalFun :: FunCtx SizeT -> OracleInterp -> [Value] -> FunDef SizeT -> Tree [Value]
-evalFun funCtx oracleF vals_in FunDef{param_binds, ret_binds, body} = (`evalStateT` params) $ do
-  _ <- evalProgram Program{funCtx, stmt = body} oracleF
-  mapM lookupVar ret_names
+evalFun funCtx oracleF vals_in FunDef{param_binds, ret_binds, body} =
+  (`evalStateT` params) $ do
+    evalStmt funCtx oracleF body
+    mapM lookupVar ret_names
   where
     param_names = map fst param_binds
     params = M.fromList $ zip param_names vals_in
     ret_names = map fst ret_binds
+
+evalProgram :: Program SizeT -> OracleInterp -> Evaluator ()
+evalProgram Program{funCtx, stmt} oracleF = evalStmt funCtx oracleF stmt
