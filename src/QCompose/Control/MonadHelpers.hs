3d05e86abc7a1a0662032556b802b8e09377fa93
Anurudh Peduri <anurudhp97@gmail.com>
2025-04-01 23:15:52 +0200

    cleanup code

--- src/QCompose/Control/MonadHelpers.hs
+++ src/QCompose/Control/MonadHelpers.hs
@@ -2,8 +2,9 @@
 {-# LANGUAGE Rank2Types #-}
 
 module QCompose.Control.MonadHelpers (
-  withSandbox,
   withSandboxOf,
+  withSandbox,
+  withFrozenStateOf,
   withFrozenState,
   embedStateT,
   embedReaderT,
@@ -15,19 +16,11 @@ module QCompose.Control.MonadHelpers (
 import Control.Monad.Except (MonadError, catchError, throwError)
 import Control.Monad.RWS (RWST (..))
 import Control.Monad.Reader (ReaderT (..), runReaderT)
-import Control.Monad.State (MonadState, StateT, get, put, runStateT)
+import Control.Monad.State (MonadState, StateT, runStateT)
 import Control.Monad.Trans (lift)
 import Control.Monad.Writer (WriterT (..))
 import Lens.Micro
 import Lens.Micro.Mtl
-
--- | Save the current state, run a computation and restore the saved state.
-withSandbox :: (MonadState s m) => m a -> m a
-withSandbox m = do
-  s <- get
-  a <- m
-  put s
-  return a
 
 -- | Save the current state, run a computation and restore the saved state.
 withSandboxOf :: (MonadState s m) => Lens' s s' -> m a -> m a
@@ -37,33 +30,44 @@ withSandboxOf part action = do
   part .= s
   return a
 
+-- | Save the current state, run a computation and restore the saved state.
+withSandbox :: (MonadState s m) => m a -> m a
+withSandbox = withSandboxOf id
+
 -- | Run a computation with the current state as a read-only environment.
-withFrozenState :: (Monad m) => ReaderT s m a -> StateT s m a
-withFrozenState m = do
-  s <- get
+withFrozenStateOf :: (Monad m) => Lens' s s' -> ReaderT s' m a -> StateT s m a
+withFrozenStateOf part m = do
+  s <- use part
   lift $ runReaderT m s
 
+-- | Run a computation with the current state as a read-only environment.
+withFrozenState :: (Monad m) => ReaderT s m a -> StateT s m a
+withFrozenState = withFrozenStateOf id
+
+-- | Embed a state computation into an RWS monad.
 embedStateT :: (Monad m, Monoid w) => StateT s m a -> RWST r w s m a
 embedStateT m = RWST $ \_ s -> do
   (a, s') <- runStateT m s
   return (a, s', mempty)
 
+-- | Embed a reader computation into an RWS monad.
 embedReaderT :: (Monad m, Monoid w) => ReaderT r m a -> RWST r w s m a
 embedReaderT m = RWST $ \r s -> do
   a <- runReaderT m r
   return (a, s, mempty)
 
+-- | Embed a writer computation into an RWS monad.
 embedWriterT :: (Monad m, Monoid w) => WriterT w m a -> RWST r w s m a
 embedWriterT m = RWST $ \_ s -> do
   (a, w) <- runWriterT m
   return (a, s, w)
 
+-- | try-catch block that prepends a message to the existing error
 throwFrom :: (MonadError String m) => m a -> String -> m a
 throwFrom action msg =
   action `catchError` \e ->
-    throwError $
-      unlines
-        [msg, "caught while handling exception:", e]
+    throwError $ unlines [msg, "caught while handling exception:", e]
 
+-- | lift a @Maybe@ to a value, throwing an error if @Nothing@
 maybeWithError :: (MonadError e m) => e -> Maybe a -> m a
 maybeWithError err = maybe (throwError err) return
