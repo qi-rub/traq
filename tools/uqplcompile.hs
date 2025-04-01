27f4e4cb2471cd30e19edca3da6c9c610d0f6a75
Anurudh Peduri <anurudhp97@gmail.com>
2025-04-01 18:25:31 +0200

    use optparse for uqplcompile

--- tools/uqplcompile.hs
+++ tools/uqplcompile.hs
@@ -1,8 +1,10 @@
-module Main where
+module Main (main) where
 
 import Control.Monad (forM_)
+import Control.Monad.Trans.Writer
 import qualified Data.Number.Symbolic as Sym
 import Lens.Micro
+import Options.Applicative
 
 import qualified QCompose.Data.Context as Ctx
 
@@ -12,21 +14,63 @@ import qualified QCompose.ProtoLang as P
 import qualified QCompose.UnitaryQPL as UQPL
 import QCompose.Utils.Printing
 
-subsNM :: SizeT -> SizeT -> (SymbSize -> SizeT)
-subsNM n m = Sym.unSym . Sym.subst "N" (Sym.con n) . Sym.subst "M" (Sym.con m)
+data Options = Options
+  { in_file :: FilePath
+  , out_file :: FilePath
+  , delta :: Float
+  }
+  deriving (Show)
+
+opts :: ParserInfo Options
+opts =
+  info
+    (options <**> helper)
+    ( fullDesc
+        <> header "Compile Proto programs to UQPL and analyze their costs."
+    )
+ where
+  options =
+    Options
+      <$> strOption (long "input" <> short 'i' <> metavar "FILENAME" <> help "Input file")
+      <*> strOption (long "output" <> short 'o' <> metavar "FILENAME" <> help "Output file")
+      <*> option
+        auto
+        ( long "precision"
+            <> short 'p'
+            <> metavar "FLOAT"
+            <> help "The precision to lower the whole program"
+        )
+
+subsNM :: (SymbSize -> SizeT)
+subsNM = Sym.unSym . Sym.subst "N" (Sym.con n) . Sym.subst "M" (Sym.con m)
+ where
+  n = 20
+  m = 10
 
 main :: IO ()
 main = do
-  code <- readFile "examples/matrix_search/matrix_search.qb"
-  let n = 20
-  let m = 10
-  let Right prog = fmap (subsNM n m) <$> P.parseProgram code
-  let delta = 0.001
-  let Right (uqpl_prog, _) = UQPL.lowerProgram zalkaQSearch Ctx.empty delta prog
-  let (cost, proc_costs) = UQPL.programCost uqpl_prog
-  forM_ (uqpl_prog ^. to UQPL.proc_defs) $ \p -> do
-    putStrLn $ "// Cost: " <> show (proc_costs ^. at (p ^. to UQPL.proc_name) . singular _Just)
-    putStrLn $ toCodeString p
+  Options{..} <- execParser opts
 
-  putStrLn $ "// Cost: " <> show cost
-  putStrLn $ toCodeString $ uqpl_prog ^. to UQPL.stmt
+  -- parse
+  code <- readFile in_file
+  let Right prog = fmap subsNM <$> P.parseProgram code
+
+  -- compile
+  let Right (uqpl_prog, _) = UQPL.lowerProgram zalkaQSearch Ctx.empty delta prog
+  -- get costs
+  let (cost, proc_costs) = UQPL.programCost uqpl_prog
+
+  -- print the program with the costs
+  out_prog <- execWriterT $ do
+    forM_ (uqpl_prog ^. to UQPL.proc_defs) $ \p -> do
+      tell $ "// Cost: " <> show (proc_costs ^. at (p ^. to UQPL.proc_name) . singular _Just)
+      tell "\n"
+      tell $ toCodeString p
+      tell "\n"
+
+    tell $ "// Cost: " <> show cost
+    tell "\n"
+    tell $ toCodeString $ uqpl_prog ^. to UQPL.stmt
+    tell "\n"
+
+  writeFile out_file out_prog
