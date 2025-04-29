d7d5297b9f247598e13206d016078c7e29663943
Anurudh Peduri <anurudhp97@gmail.com>
2025-04-29 20:44:43 +0200

    remove unused code

--- expt/Algorithms/Majority.hs
+++
@@ -1,9 +0,0 @@
-module QCompose.Algorithms.Majority (majority) where
-
-import QCompose.Algorithms.Basic (Unitary)
-import Quipper (Qubit)
-
-majority :: [Unitary (qa, Qubit)] -> Unitary (qa, Qubit, [qa], [Qubit])
-majority us = \(a, f, as, fs) -> do
-    undefined
-
