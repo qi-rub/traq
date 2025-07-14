f079f56e68e0032c467f520b57c4c93348f0c091
Anurudh Peduri <anurudhp97@gmail.com>
2025-07-14 22:20:58 +0200

    more support for CQPL worst case cost

--- src/Traq/CQPL/Cost.hs
+++ src/Traq/CQPL/Cost.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 module Traq.CQPL.Cost (
-  stmtCost,
   procCost,
   programCost,
 
@@ -45,39 +44,79 @@ type CostCalculator holeT sizeT costT =
 
 -- | Compute the cost of a placeholder (hole) program.
 class HoleCost holeT costT where
-  holeCost :: forall sizeT. (Integral sizeT, Floating costT) => holeT -> CostCalculator holeT sizeT costT costT
+  holeCost ::
+    forall sizeT m.
+    ( Integral sizeT
+    , Floating costT
+    , Ord costT
+    , m ~ CostCalculator holeT sizeT costT
+    ) =>
+    holeT ->
+    m costT
 
 instance HoleCost Void costT where
   holeCost = absurd
 
-stmtCost ::
+ustmtCost ::
   ( Integral sizeT
   , Floating costT
+  , Ord costT
   , HoleCost holeT costT
   , m ~ CostCalculator holeT sizeT costT
   ) =>
   UStmt holeT sizeT ->
   m costT
-stmtCost USkipS = return 0
-stmtCost (UCommentS _) = return 0
-stmtCost UnitaryS{} = return 0
-stmtCost UCallS{uproc_id} = procCost uproc_id
-stmtCost (USeqS ss) = sum <$> mapM stmtCost ss
-stmtCost URepeatS{n_iter = P.MetaSize k, uloop_body} = (fromIntegral k *) <$> stmtCost uloop_body
-stmtCost URepeatS{n_iter = P.MetaValue k, uloop_body} = (fromIntegral k *) <$> stmtCost uloop_body
-stmtCost URepeatS{n_iter = P.MetaName _} = throwError "unsupported meta parameter substitution"
-stmtCost UHoleS{uhole} = holeCost uhole
-stmtCost UForInRangeS{iter_lim = P.MetaSize k, uloop_body} = (fromIntegral k *) <$> stmtCost uloop_body
-stmtCost UForInRangeS{iter_lim = P.MetaValue k, uloop_body} = (fromIntegral k *) <$> stmtCost uloop_body
-stmtCost UForInRangeS{iter_lim = _} = throwError "unsupported meta parameter substitution"
-stmtCost UWithComputedS{with_ustmt, body_ustmt} = do
-  wc <- stmtCost with_ustmt
-  bc <- stmtCost body_ustmt
+ustmtCost USkipS = return 0
+ustmtCost (UCommentS _) = return 0
+ustmtCost UnitaryS{} = return 0
+ustmtCost UCallS{uproc_id} = procCost uproc_id
+ustmtCost (USeqS ss) = sum <$> mapM ustmtCost ss
+ustmtCost URepeatS{n_iter = P.MetaSize k, uloop_body} = (fromIntegral k *) <$> ustmtCost uloop_body
+ustmtCost URepeatS{n_iter = P.MetaValue k, uloop_body} = (fromIntegral k *) <$> ustmtCost uloop_body
+ustmtCost URepeatS{n_iter = P.MetaName _} = throwError "unsupported meta parameter substitution"
+ustmtCost UHoleS{uhole} = holeCost uhole
+ustmtCost UForInRangeS{iter_lim = P.MetaSize k, uloop_body} = (fromIntegral k *) <$> ustmtCost uloop_body
+ustmtCost UForInRangeS{iter_lim = P.MetaValue k, uloop_body} = (fromIntegral k *) <$> ustmtCost uloop_body
+ustmtCost UForInRangeS{iter_lim = _} = throwError "unsupported meta parameter substitution"
+ustmtCost UWithComputedS{with_ustmt, body_ustmt} = do
+  wc <- ustmtCost with_ustmt
+  bc <- ustmtCost body_ustmt
   return $ 2 * wc + bc
+
+stmtCost ::
+  ( Integral sizeT
+  , Floating costT
+  , Ord costT
+  , HoleCost holeT costT
+  , m ~ CostCalculator holeT sizeT costT
+  ) =>
+  Stmt holeT sizeT ->
+  m costT
+-- zero-cost statements
+stmtCost SkipS = return 0
+stmtCost (CommentS _) = return 0
+stmtCost AssignS{} = return 0
+stmtCost RandomS{} = return 0
+stmtCost RandomDynS{} = return 0
+-- single statements
+stmtCost CallS{fun = FunctionCall p, meta_params = []} = procCost p
+stmtCost CallS{fun = UProcAndMeas up, meta_params = []} = procCost up
+stmtCost CallS{} = throwError "unsupported cost: proc call with params"
+stmtCost (HoleS h) = holeCost h
+-- compound statements
+stmtCost (SeqS ss) = sum <$> mapM stmtCost ss
+stmtCost IfThenElseS{s_true, s_false} = max <$> stmtCost s_true <*> stmtCost s_false
+stmtCost RepeatS{n_iter = P.MetaSize k, loop_body} = (fromIntegral k *) <$> stmtCost loop_body
+stmtCost RepeatS{n_iter = P.MetaValue k, loop_body} = (fromIntegral k *) <$> stmtCost loop_body
+stmtCost RepeatS{} = throwError "unsupported cost"
+stmtCost WhileK{} = throwError "unsupported cost"
+stmtCost WhileKWithCondExpr{} = throwError "unsupported cost"
+stmtCost ForInArray{} = throwError "unsupported cost"
 
 procCost ::
   ( Integral sizeT
   , Floating costT
+  , Ord costT
   , HoleCost holeT costT
   , m ~ CostCalculator holeT sizeT costT
   ) =>
@@ -94,20 +133,22 @@ procCost name = get_cached_cost >>= maybe calc_cost return
       ProcBodyC cproc_body ->
         case cproc_body of
           CProcDecl{ctick} -> pure ctick
-          CProcBody{cproc_body_stmt} ->
-            throwError "TODO: implement worst-case cost for classical Stmt"
+          CProcBody{cproc_body_stmt} -> stmtCost cproc_body_stmt
       ProcBodyU uproc_body ->
         case uproc_body of
           UProcDecl{utick} -> pure utick
-          UProcBody{uproc_body_stmt} -> stmtCost uproc_body_stmt
+          UProcBody{uproc_body_stmt} -> ustmtCost uproc_body_stmt
     at name ?= cost
     return cost
 
 programCost ::
-  (Integral sizeT, Floating costT, HoleCost holeT costT) =>
+  ( Integral sizeT
+  , Floating costT
+  , Ord costT
+  , HoleCost holeT costT
+  ) =>
   Program holeT sizeT costT ->
   (costT, CostMap costT)
 programCost Program{proc_defs} = fromRight (error "could not compute cost") $ do
-  -- TODO support cost for procs
   let env = proc_defs
   runMyReaderStateT (procCost "main") env Map.empty
