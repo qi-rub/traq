06cba495bf29dd050f63aa714965d6e27e642061
Anurudh Peduri <anurudhp97@gmail.com>
2026-01-15 12:53:54 +0100

    clean

--- src/Traq/Primitives/Class.hs
+++ src/Traq/Primitives/Class.hs
@@ -1,39 +1,10 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE UndecidableInstances #-}
-
 module Traq.Primitives.Class (
-  -- * Primitives
-  Primitive (..),
-  PrimFnShape,
-  ValidPrimShape (..),
-  reshapeUnsafe,
-
-  -- ** Typeclasses
-  SerializePrim (..),
-  TypeCheckPrim (..),
-  EvalPrim (..),
-
-  -- ** Unitary Compilation and Cost
-  UnitaryCostPrim (..),
-  UnitaryQueries (..),
-  strongQueries,
-  weakQueries,
-  zeroQ,
-
-  -- ** Classical-Quantum Compilation and Cost
-  QuantumHavocCostPrim (..),
-  QuantumExpCostPrim (..),
-
-  -- ** Partial Functions
-  PartialFun (..),
-  placeArgs,
+  module Traq.Primitives.Class.Prelude,
+  module Traq.Primitives.Class.Serialize,
+  module Traq.Primitives.Class.TypeCheck,
+  module Traq.Primitives.Class.Eval,
+  module Traq.Primitives.Class.Cost,
 ) where
-
--- Re-export all modules
 
 import Traq.Primitives.Class.Cost
 import Traq.Primitives.Class.Eval
