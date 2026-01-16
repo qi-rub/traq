5e98f26570d5021dac2af21a0f01587d29e62cb2
Anurudh Peduri <anurudhp97@gmail.com>
2026-01-16 13:52:31 +0100

    cleanup old costs

--- src/Traq/Analysis/Cost.hs
+++ src/Traq/Analysis/Cost.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -10,11 +9,6 @@ module Traq.Analysis.Cost (
   module Traq.Analysis.Cost.Quantum,
 
   -- * Old cost functions
-
-  -- * Unitary Cost
-  unitaryQueryCost,
-  unitaryQueryCostF,
-  UnitaryCost (..),
 
   -- * Types and Monad
   CostEnv,
@@ -28,15 +22,13 @@ module Traq.Analysis.Cost (
   splitEps,
 ) where
 
-import Control.Monad (forM, zipWithM)
-import Control.Monad.Reader (MonadReader, ReaderT, runReaderT)
+import Control.Monad (forM)
+import Control.Monad.Reader (MonadReader, ReaderT)
 import GHC.Generics hiding (to)
 
 import Lens.Micro.GHC
 import Lens.Micro.Mtl
-import qualified Numeric.Algebra as Alg
 
-import Traq.Control.Monad
 import qualified Traq.Data.Context as Ctx
 import Traq.Data.Default
 
@@ -44,22 +36,10 @@ import Traq.Analysis.Annotate.Prelude
 import Traq.Analysis.Cost.Prelude
 import Traq.Analysis.Cost.Quantum
 import Traq.Analysis.Cost.Unitary
-import qualified Traq.Analysis.CostModel.Class as C
 import Traq.Analysis.Error
-import Traq.Analysis.Prelude
 import Traq.Prelude
 import Traq.ProtoLang.Eval
 import Traq.ProtoLang.Syntax
-import Traq.ProtoLang.TypeCheck
-
-{-# DEPRECATED
-  unitaryQueryCost
-  , unitaryQueryCostE
-  , unitaryQueryCostS
-  , unitaryQueryCostF
-  , UnitaryCost
-  "Use the newer versions on annotated programs"
-  #-}
 
 -- ================================================================================
 -- Strategy for splitting the precison (eps/delta)
@@ -160,175 +140,3 @@ instance HasCostEnv (CostEnv ext) ext where _costEnv = id
 
 -- | Monad for cost analysis
 type CostCalculator ext = ReaderT (CostEnv ext) Maybe
-
--- ================================================================================
--- Unitary Cost
--- ================================================================================
-
--- --------------------------------------------------------------------------------
--- Unitary Cost: Primitives (with generics)
--- --------------------------------------------------------------------------------
-
--- | Primitives that have a unitary cost
-class
-  (TypeInferrable primT sizeT, sizeT ~ SizeType primT, precT ~ PrecType primT) =>
-  UnitaryCost primT sizeT precT
-    | primT -> sizeT precT
-  where
-  unitaryCost ::
-    forall primsT costT m.
-    ( m ~ CostCalculator primsT
-    , UnitaryCost primsT sizeT precT
-    , C.CostModel costT
-    , precT ~ PrecType costT
-    , SizeToPrec sizeT precT
-    ) =>
-    L2NormError precT ->
-    primT ->
-    m costT
-  default unitaryCost ::
-    forall primsT costT m.
-    ( m ~ CostCalculator primsT
-    , UnitaryCost primsT sizeT precT
-    , C.CostModel costT
-    , precT ~ PrecType costT
-    , SizeToPrec sizeT precT
-    , Generic primT
-    , GUnitaryCost (Rep primT) sizeT precT
-    ) =>
-    L2NormError precT ->
-    primT ->
-    m costT
-  unitaryCost delta p = gunitaryCost delta (from p)
-
-instance (TypingReqs sizeT) => UnitaryCost (Core sizeT precT) sizeT precT where
-  unitaryCost _ = \case {}
-
-class GUnitaryCost f sizeT precT where
-  gunitaryCost ::
-    forall primsT primT costT m.
-    ( m ~ CostCalculator primsT
-    , UnitaryCost primsT sizeT precT
-    , C.CostModel costT
-    , precT ~ PrecType costT
-    , SizeToPrec sizeT precT
-    ) =>
-    L2NormError precT ->
-    f primT ->
-    m costT
-
-instance
-  (GUnitaryCost f1 sizeT precT, GUnitaryCost f2 sizeT precT) =>
-  GUnitaryCost (f1 :+: f2) sizeT precT
-  where
-  gunitaryCost delta (L1 p) = gunitaryCost delta p
-  gunitaryCost delta (R1 p) = gunitaryCost delta p
-
-instance (GUnitaryCost f sizeT precT) => GUnitaryCost (M1 i c f) sizeT precT where
-  gunitaryCost delta (M1 x) = gunitaryCost delta x
-
-instance
-  (UnitaryCost f sizeT precT) =>
-  GUnitaryCost (K1 i f) sizeT precT
-  where
-  gunitaryCost delta (K1 x) = unitaryCost delta x
-
--- --------------------------------------------------------------------------------
--- Unitary Cost: Core Language
--- --------------------------------------------------------------------------------
-
--- | Evaluate the query cost of an expression
-unitaryQueryCostE ::
-  forall primsT sizeT precT costT m.
-  ( Num sizeT
-  , Floating precT
-  , UnitaryCost primsT sizeT precT
-  , m ~ CostCalculator primsT
-  , C.CostModel costT
-  , precT ~ PrecType costT
-  , SizeToPrec sizeT precT
-  ) =>
-  -- | precision
-  L2NormError precT ->
-  -- | expression @E@
-  Expr primsT ->
-  m costT
-unitaryQueryCostE delta FunCallE{fname} = ((2.0 :: precT) Alg..*) <$> unitaryQueryCostF (delta `divideError` 2) fname
-unitaryQueryCostE delta PrimCallE{prim} = unitaryCost delta prim
--- basic expressions
-unitaryQueryCostE _ BasicExprE{basic_expr} = return $ C.callExpr C.Unitary basic_expr
-unitaryQueryCostE _ RandomSampleE{distr_expr} = return $ C.callDistrExpr C.Unitary distr_expr
-unitaryQueryCostE _ _ = error "TODO"
-
--- Evaluate the query cost of a statement
-unitaryQueryCostS ::
-  forall primsT sizeT precT costT m.
-  ( Num sizeT
-  , Floating precT
-  , UnitaryCost primsT sizeT precT
-  , m ~ CostCalculator primsT
-  , C.CostModel costT
-  , precT ~ PrecType costT
-  , SizeToPrec sizeT precT
-  ) =>
-  -- | precision (l2-norm)
-  L2NormError precT ->
-  -- | statement @S@
-  Stmt primsT ->
-  m costT
-unitaryQueryCostS delta ExprS{expr} = unitaryQueryCostE delta expr
-unitaryQueryCostS delta IfThenElseS{s_true, s_false} = do
-  cost_true <- unitaryQueryCostS delta s_true
-  cost_false <- unitaryQueryCostS delta s_false
-  return $ cost_true Alg.+ cost_false
-unitaryQueryCostS delta (SeqS ss) = do
-  delta_each <- splitEps delta ss
-  cs <- zipWithM unitaryQueryCostS delta_each ss
-  return $ Alg.sum cs
-
--- Evaluate the query cost of a function
-unitaryQueryCostF ::
-  forall primsT sizeT precT costT m.
-  ( Num sizeT
-  , Floating precT
-  , UnitaryCost primsT sizeT precT
-  , m ~ CostCalculator primsT
-  , C.CostModel costT
-  , precT ~ PrecType costT
-  , SizeToPrec sizeT precT
-  ) =>
-  -- | precision (l2-norm)
-  L2NormError precT ->
-  -- | function name
-  Ident ->
-  m costT
--- declaration: use tick value (or 0 if not specified)
-unitaryQueryCostF delta fname = do
-  FunDef{mbody} <- view $ _funCtx . Ctx.at fname . non' (error $ "invalid function: " ++ fname)
-  case mbody of
-    Nothing -> return $ C.query C.Unitary fname -- query an external function
-    Just FunBody{body_stmt} -> unitaryQueryCostS delta body_stmt -- def: compute using body
-
-unitaryQueryCost ::
-  forall primsT sizeT precT costT.
-  ( Num sizeT
-  , Floating precT
-  , UnitaryCost primsT sizeT precT
-  , C.CostModel costT
-  , precT ~ PrecType costT
-  , SizeToPrec sizeT precT
-  ) =>
-  PrecisionSplittingStrategy ->
-  -- | precision (l2-norm)
-  L2NormError precT ->
-  -- | program @P@
-  Program primsT ->
-  costT
-unitaryQueryCost strat delta (Program fs) =
-  let env =
-        default_
-          & (_funCtx .~ namedFunsToFunCtx fs)
-          & (_precSplitStrat .~ strat)
-   in unitaryQueryCostF delta (fun_name $ last fs)
-        & (runReaderT ?? env)
-        & (^?! _Just)
