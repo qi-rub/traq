6eec3646c203133500d967962c8ea51a3c8e3075
Anurudh Peduri <anurudhp97@gmail.com>
2026-02-17 20:20:16 +0000

    wip

--- test/Traq/Examples/SteepMaxKSatSpec.hs
+++ test/Traq/Examples/SteepMaxKSatSpec.hs
@@ -43,16 +43,16 @@ spec = describe "Steep max-k-sat" $ do
     ex <- loadExample
     assertRight $ P.typeCheckProg ex
 
-  xdescribe "Compile" $ do
+  fdescribe "Compile" $ do
     let eps = A.failProb (0.0001 :: Double)
 
     it "lowers" $ do
-      ex <- loadExample
+      ex <- P.renameVars' <$> loadExample
       ex' <- expectRight $ A.annotateProgWith (P._exts (A.annSinglePrim eps)) ex
       assertRight $ Compiler.lowerProgram ex'
 
     it "typechecks" $ do
-      ex <- loadExample
+      ex <- P.renameVars' <$> loadExample
       ex' <- expectRight $ A.annotateProgWith (P._exts (A.annSinglePrim eps)) ex
       ex_uqpl <- expectRight $ Compiler.lowerProgram ex'
       assertRight $ CQPL.typeCheckProgram ex_uqpl
