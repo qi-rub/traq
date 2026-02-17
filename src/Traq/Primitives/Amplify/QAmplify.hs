ee56e59ec20b771893d428f8ceaebe5b843d3554
Anurudh Peduri <anurudhp97@gmail.com>
2026-02-17 16:53:27 +0000

    wip qamp

--- src/Traq/Primitives/Amplify/QAmplify.hs
+++ src/Traq/Primitives/Amplify/QAmplify.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
@@ -11,7 +12,7 @@ module Traq.Primitives.Amplify.QAmplify (
   _EQSearch,
 ) where
 
-import Control.Monad (forM)
+import Control.Monad (forM, replicateM)
 import Control.Monad.Trans (lift)
 import GHC.Generics (Generic)
 import Text.Printf (printf)
@@ -190,24 +191,62 @@ instance (Floating prec, RealFrac prec) => UnitaryCompilePrim (QAmplify size pre
 -- --------------------------------------------------------------------------------
 -- CQ Compilation
 -- --------------------------------------------------------------------------------
+
+-- | Run K grover iterations
+mkGroverK ::
+  forall ext size prec m.
+  ( m ~ PrimCompileMonad ext (QAmplify size prec)
+  , size ~ SizeType ext
+  ) =>
+  m (CQPL.ProcDef size)
+mkGroverK = do
+  proc_name <- Compiler.newIdent "Grover"
+  meta_k <- Compiler.newIdent "k"
+
+  rets <- view $ to ret_vars
+  let b = head rets
+  ret_tys <- forM rets $ \x -> do
+    error "return type of x"
+
+  (SamplerFn mk_sampler_call) <- view $ to mk_ucall
+  (SamplerFn aux_tys) <- view $ to uproc_aux_types
+  aux_vars <- replicateM (length aux_tys) $ Compiler.newIdent "aux"
+
+  let sampler_call = mk_sampler_call (map CQPL.Arg (rets ++ aux_vars))
+
+  let grover_iteration =
+        CQPL.USeqS
+          [ CQPL.UnitaryS [CQPL.Arg b] $ CQPL.BasicGateU CQPL.ZGate
+          , CQPL.adjoint sampler_call
+          , CQPL.UnitaryS (map CQPL.Arg (rets ++ aux_vars)) $ CQPL.BasicGateU (CQPL.PhaseOnZero pi)
+          , sampler_call
+          ]
+
+  let uproc_body_stmt =
+        CQPL.USeqS
+          [ sampler_call
+          , CQPL.URepeatS{n_iter = P.MetaName meta_k, uloop_body = grover_iteration}
+          ]
+
+  let params =
+        Compiler.withTag CQPL.ParamOut (zip rets ret_tys)
+          ++ Compiler.withTag CQPL.ParamAux (zip aux_vars aux_tys)
+  return $
+    CQPL.ProcDef
+      { CQPL.info_comment = ""
+      , CQPL.proc_name
+      , CQPL.proc_meta_params = [meta_k]
+      , CQPL.proc_param_types = map (view _3) params
+      , CQPL.proc_body =
+          CQPL.ProcBodyU $
+            CQPL.UProcBody
+              { CQPL.uproc_param_names = map (view _1) params
+              , CQPL.uproc_param_tags = map (view _2) params
+              , CQPL.uproc_body_stmt
+              }
+      }
+
 {-
-
-// function argument
-uproc g(b, y, $\veca$) ...
-
-// run k grover iterations
-uproc grover$_k$(b, y, $\veca$) do {
-  call g(b, y, $\veca$);
-
-  repeat $k$ {
-    b *= Z;
-
-    call$^\dagger$ g(b, y, $\veca$);
-    b, y, $\veca$ *= PhaseOnZero($\pi$); // reflect about $\ket{0}$ (with global phase -1)
-    call g(b, y, $\veca$);
-  }
-}
-
 uproc QAmplify(b, y) do {
   not_done := 0 : Bool;
   repeat $N_\text{runs}$ {
@@ -225,67 +264,9 @@ uproc QAmplify(b, y) do {
 }
 -}
 
--- | the generated QSearch procedure: body stmts and local vars
-type QSearchCompilerT ext =
-  WriterT
-    ([CQPL.Stmt (SizeType ext)], [(Ident, P.VarType (SizeType ext))])
-    (Compiler.CompilerT ext)
-
-allocReg ::
-  ( m ~ QSearchCompilerT ext
-  , sizeT ~ SizeType ext
-  ) =>
-  Ident ->
-  P.VarType sizeT ->
-  m Ident
-allocReg prefix ty = do
-  reg <- lift $ Compiler.newIdent prefix
-  writeElemAt _2 (reg, ty)
-  return reg
-
--- | Run K grover iterations
-groverK ::
-  forall sizeT.
-  -- | number of rounds
-  P.MetaParam sizeT ->
-  -- | the element and type to search for. @x : T@
-  (Ident, P.VarType sizeT) ->
-  -- | the output bit
-  Ident ->
-  -- | run the predicate
-  (Ident -> Ident -> CQPL.UStmt sizeT) ->
-  CQPL.UStmt sizeT
-groverK k (x, x_ty) b mk_pred =
-  CQPL.USeqS
-    [ prepb
-    , prepx
-    , CQPL.URepeatS k grover_iterate
-    , CQPL.adjoint prepb
-    ]
- where
-  unifX = CQPL.DistrU (P.UniformE x_ty)
-
-  -- map b to |-> and x to uniform
-  prepb, prepx :: CQPL.UStmt sizeT
-  prepb =
-    CQPL.USeqS
-      [ CQPL.UnitaryS [CQPL.Arg b] (CQPL.BasicGateU CQPL.XGate)
-      , CQPL.UnitaryS [CQPL.Arg b] (CQPL.BasicGateU CQPL.HGate)
-      ]
-  prepx = CQPL.UnitaryS [CQPL.Arg x] unifX
-
-  grover_iterate :: CQPL.UStmt sizeT
-  grover_iterate =
-    CQPL.USeqS
-      [ mk_pred x b
-      , CQPL.UnitaryS [CQPL.Arg x] (CQPL.Adjoint unifX)
-      , CQPL.UnitaryS [CQPL.Arg x] (CQPL.BasicGateU (CQPL.PhaseOnZero pi))
-      , CQPL.UnitaryS [CQPL.Arg x] unifX
-      ]
-
 -- | Implementation of the hybrid quantum search algorithm \( \textbf{QSearch} \).
 algoQSearch ::
-  forall ext sizeT precT.
+  forall ext sizeT precT m.
   ( Integral sizeT
   , RealFloat precT
   , sizeT ~ SizeT
@@ -293,6 +274,7 @@ algoQSearch ::
   , Show precT
   , P.TypingReqs sizeT
   , SizeType ext ~ sizeT
+  , m ~ PrimCompileMonad ext (QAmplify sizeT precT)
   ) =>
   -- | search elem type
   P.VarType sizeT ->
@@ -309,7 +291,7 @@ algoQSearch ::
   -- | output value
   Ident ->
   -- | the generated QSearch procedure: body stmts and local vars
-  QSearchCompilerT ext ()
+  m ()
 algoQSearch ty n_samples eps grover_k_caller pred_caller ok x = do
   not_done <- allocReg "not_done" P.tbool
   q_sum <- allocReg "Q_sum" j_type
@@ -394,97 +376,97 @@ algoQSearch ty n_samples eps grover_k_caller pred_caller ok x = do
     nxt :: Float -> Float
     nxt m = min (lambda * m) sqrt_n
 
-instance
-  (RealFloat prec, Show prec) =>
-  QuantumCompilePrim (QSearchCFNW SizeT prec) SizeT prec
-  where
-  compileQPrim (QSearchCFNW PrimSearch{search_kind, search_ty}) eps = do
-    -- lowered unitary predicate
-    (BooleanPredicate call_upred) <- view $ to mk_ucall
-    (BooleanPredicate pred_aux_tys) <- view $ to uproc_aux_types
+-- instance
+--   (RealFloat prec, Show prec) =>
+--   QuantumCompilePrim (QSearchCFNW SizeT prec) SizeT prec
+--   where
+--   compileQPrim (QSearchCFNW PrimSearch{search_kind, search_ty}) eps = do
+--     -- lowered unitary predicate
+--     (BooleanPredicate call_upred) <- view $ to mk_ucall
+--     (BooleanPredicate pred_aux_tys) <- view $ to uproc_aux_types
 
-    (ret, x_out) <- case search_kind of
-      SearchK -> do
-        view (to ret_vars) >>= \case
-          [b, x_out] -> pure (b, x_out)
-          _ -> throwError "search must return (bool, T)"
-      _ -> do
-        b <-
-          view (to ret_vars) >>= \case
-            [b] -> pure b
-            _ -> throwError "bool predicate must return single bool"
-        x_out <- lift $ Compiler.allocAncillaWithPref "s_result" search_ty
-        return (b, x_out)
+--     (ret, x_out) <- case search_kind of
+--       SearchK -> do
+--         view (to ret_vars) >>= \case
+--           [b, x_out] -> pure (b, x_out)
+--           _ -> throwError "search must return (bool, T)"
+--       _ -> do
+--         b <-
+--           view (to ret_vars) >>= \case
+--             [b] -> pure b
+--             _ -> throwError "bool predicate must return single bool"
+--         x_out <- lift $ Compiler.allocAncillaWithPref "s_result" search_ty
+--         return (b, x_out)
 
-    -- make the Grover_k uproc
-    -- TODO this should ideally be done by algoQSearch, but requires a lot of aux information.
-    uproc_grover_k_name <- Compiler.newIdent "Grover"
-    upred_aux_vars <- replicateM (length pred_aux_tys) $ Compiler.newIdent "aux"
-    grover_arg_name <- Compiler.newIdent "x"
-    let meta_k = P.MetaName "k"
-    let uproc_grover_k_body =
-          groverK
-            meta_k
-            (grover_arg_name, search_ty)
-            ret
-            (\x b -> call_upred (map CQPL.Arg ([x, b] ++ upred_aux_vars)))
-    let uproc_grover_k_params =
-          Compiler.withTag CQPL.ParamInp [(grover_arg_name, search_ty)]
-            ++ Compiler.withTag CQPL.ParamOut [(ret, P.tbool)]
-            ++ Compiler.withTag CQPL.ParamAux (zip upred_aux_vars pred_aux_tys)
-    let uproc_grover_k =
-          CQPL.ProcDef
-            { CQPL.info_comment = "Grover[...]"
-            , CQPL.proc_name = uproc_grover_k_name
-            , CQPL.proc_meta_params = ["k"]
-            , CQPL.proc_param_types = map (view _3) uproc_grover_k_params
-            , CQPL.proc_body =
-                CQPL.ProcBodyU $
-                  CQPL.UProcBody
-                    { CQPL.uproc_param_names = map (view _1) uproc_grover_k_params
-                    , CQPL.uproc_param_tags = map (view _2) uproc_grover_k_params
-                    , CQPL.uproc_body_stmt = uproc_grover_k_body
-                    }
-            }
-    Compiler.addProc uproc_grover_k
+--     -- make the Grover_k uproc
+--     -- TODO this should ideally be done by algoQSearch, but requires a lot of aux information.
+--     uproc_grover_k_name <- Compiler.newIdent "Grover"
+--     upred_aux_vars <- replicateM (length pred_aux_tys) $ Compiler.newIdent "aux"
+--     grover_arg_name <- Compiler.newIdent "x"
+--     let meta_k = P.MetaName "k"
+--     let uproc_grover_k_body =
+--           groverK
+--             meta_k
+--             (grover_arg_name, search_ty)
+--             ret
+--             (\x b -> call_upred (map CQPL.Arg ([x, b] ++ upred_aux_vars)))
+--     let uproc_grover_k_params =
+--           Compiler.withTag CQPL.ParamInp [(grover_arg_name, search_ty)]
+--             ++ Compiler.withTag CQPL.ParamOut [(ret, P.tbool)]
+--             ++ Compiler.withTag CQPL.ParamAux (zip upred_aux_vars pred_aux_tys)
+--     let uproc_grover_k =
+--           CQPL.ProcDef
+--             { CQPL.info_comment = "Grover[...]"
+--             , CQPL.proc_name = uproc_grover_k_name
+--             , CQPL.proc_meta_params = ["k"]
+--             , CQPL.proc_param_types = map (view _3) uproc_grover_k_params
+--             , CQPL.proc_body =
+--                 CQPL.ProcBodyU $
+--                   CQPL.UProcBody
+--                     { CQPL.uproc_param_names = map (view _1) uproc_grover_k_params
+--                     , CQPL.uproc_param_tags = map (view _2) uproc_grover_k_params
+--                     , CQPL.uproc_body_stmt = uproc_grover_k_body
+--                     }
+--             }
+--     Compiler.addProc uproc_grover_k
 
-    let grover_k_caller k x b =
-          CQPL.CallS
-            { CQPL.fun = CQPL.UProcAndMeas uproc_grover_k_name
-            , CQPL.meta_params = [k]
-            , CQPL.args = [CQPL.Arg x, CQPL.Arg b]
-            }
+--     let grover_k_caller k x b =
+--           CQPL.CallS
+--             { CQPL.fun = CQPL.UProcAndMeas uproc_grover_k_name
+--             , CQPL.meta_params = [k]
+--             , CQPL.args = [CQPL.Arg x, CQPL.Arg b]
+--             }
 
-    -- emit the QSearch algorithm
-    let qsearch_params =
-          case search_kind of
-            SearchK -> [(ret, P.tbool), (x_out, search_ty)]
-            _ -> [(ret, P.tbool)]
+--     -- emit the QSearch algorithm
+--     let qsearch_params =
+--           case search_kind of
+--             SearchK -> [(ret, P.tbool), (x_out, search_ty)]
+--             _ -> [(ret, P.tbool)]
 
-    (BooleanPredicate meas_upred) <- view $ to mk_meas
-    let pred_caller x b = meas_upred [x, b]
+--     (BooleanPredicate meas_upred) <- view $ to mk_meas
+--     let pred_caller x b = meas_upred [x, b]
 
-    (qsearch_body, qsearch_local_vars) <-
-      lift $
-        execWriterT $
-          algoQSearch search_ty 0 eps grover_k_caller pred_caller ret x_out
+--     (qsearch_body, qsearch_local_vars) <-
+--       lift $
+--         execWriterT $
+--           algoQSearch search_ty 0 eps grover_k_caller pred_caller ret x_out
 
-    let prim_name = (case search_kind of AnyK -> "QAny"; AllK -> "QAll"; SearchK -> "QSearch")
-    qsearch_proc_name <- Compiler.newIdent prim_name
-    return
-      CQPL.ProcDef
-        { CQPL.info_comment = printf "%s[%s]" prim_name (show $ A.getFailProb eps)
-        , CQPL.proc_name = qsearch_proc_name
-        , CQPL.proc_meta_params = []
-        , CQPL.proc_param_types = map snd qsearch_params
-        , CQPL.proc_body =
-            CQPL.ProcBodyC $
-              CQPL.CProcBody
-                { CQPL.cproc_param_names = map fst qsearch_params
-                , CQPL.cproc_local_vars = qsearch_local_vars ++ [(x_out, search_ty) | search_kind /= SearchK]
-                , CQPL.cproc_body_stmt = CQPL.SeqS qsearch_body
-                }
-        }
+--     let prim_name = (case search_kind of AnyK -> "QAny"; AllK -> "QAll"; SearchK -> "QSearch")
+--     qsearch_proc_name <- Compiler.newIdent prim_name
+--     return
+--       CQPL.ProcDef
+--         { CQPL.info_comment = printf "%s[%s]" prim_name (show $ A.getFailProb eps)
+--         , CQPL.proc_name = qsearch_proc_name
+--         , CQPL.proc_meta_params = []
+--         , CQPL.proc_param_types = map snd qsearch_params
+--         , CQPL.proc_body =
+--             CQPL.ProcBodyC $
+--               CQPL.CProcBody
+--                 { CQPL.cproc_param_names = map fst qsearch_params
+--                 , CQPL.cproc_local_vars = qsearch_local_vars ++ [(x_out, search_ty) | search_kind /= SearchK]
+--                 , CQPL.cproc_body_stmt = CQPL.SeqS qsearch_body
+--                 }
+--         }
 
 instance (Floating prec, RealFrac prec) => QuantumCompilePrim (QAmplify size prec) size prec where
   compileQPrim (QAmplify Amplify{p_min}) eps = do
