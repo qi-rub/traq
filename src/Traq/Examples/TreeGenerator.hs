03a6ae9fbafd5d987faa114e2e52c1d89a288aeb
Anurudh Peduri <anurudhp97@gmail.com>
2026-03-17 09:45:58 +0000

    fix

--- src/Traq/Examples/TreeGenerator.hs
+++ src/Traq/Examples/TreeGenerator.hs
@@ -54,30 +54,6 @@ treeGeneratorExample n w p k =
               }
         }
     , NamedFunDef
-        { fun_name = "AddWeight"
-        , fun_def =
-            FunDef
-              { param_types = [Arr n (Fin 2), Fin w, Fin n]
-              , ret_types = [Arr n (Fin 2), Fin w]
-              , mbody =
-                  Just
-                    ( FunBody
-                        { param_names = ["xs", "cur_wt", "i"]
-                        , ret_names = ["xs'", "new_wt"]
-                        , body_stmt =
-                            SeqS
-                              [ ExprS{rets = ["xi"], expr = BasicExprE{basic_expr = DynIndexE{arr_expr = VarE{var = "xs"}, ix_expr = VarE{var = "i"}}}}
-                              , ExprS{rets = ["wi"], expr = FunCallE{fname = "Weight", args = ["i"]}}
-                              , ExprS{rets = ["zero"], expr = BasicExprE{basic_expr = ConstE{val = FinV 0, ty = Fin w}}}
-                              , ExprS{rets = ["wi_pick"], expr = BasicExprE{basic_expr = TernaryE{branch = VarE{var = "xi"}, lhs = VarE{var = "wi"}, rhs = VarE{var = "zero"}}}}
-                              , ExprS{rets = ["new_wt"], expr = BasicExprE{basic_expr = BinOpE{bin_op = AddOp, lhs = VarE{var = "cur_wt"}, rhs = VarE{var = "wi_pick"}}}}
-                              , ExprS{rets = ["xs'"], expr = BasicExprE{basic_expr = VarE{var = "xs"}}}
-                              ]
-                        }
-                    )
-              }
-        }
-    , NamedFunDef
         { fun_name = "TotalWeight"
         , fun_def =
             FunDef
@@ -85,40 +61,26 @@ treeGeneratorExample n w p k =
               , ret_types = [Fin w]
               , mbody =
                   Just
-                    ( FunBody
-                        { param_names = ["xs"]
-                        , ret_names = ["wt"]
-                        , body_stmt =
-                            SeqS
-                              [ ExprS{rets = ["zero"], expr = BasicExprE{basic_expr = ConstE{val = FinV 0, ty = Fin w}}}
-                              , ExprS{rets = ["xs'", "wt"], expr = LoopE{initial_args = ["xs", "zero"], loop_body_fun = "AddWeight"}}
-                              ]
-                        }
-                    )
-              }
-        }
-    , NamedFunDef
-        { fun_name = "AddProfit"
-        , fun_def =
-            FunDef
-              { param_types = [Arr n (Fin 2), Fin p, Fin n]
-              , ret_types = [Arr n (Fin 2), Fin p]
-              , mbody =
-                  Just
-                    ( FunBody
-                        { param_names = ["xs", "cur_pr", "i"]
-                        , ret_names = ["xs'", "new_pr"]
-                        , body_stmt =
-                            SeqS
-                              [ ExprS{rets = ["xi"], expr = BasicExprE{basic_expr = DynIndexE{arr_expr = VarE{var = "xs"}, ix_expr = VarE{var = "i"}}}}
-                              , ExprS{rets = ["pi"], expr = FunCallE{fname = "Profit", args = ["i"]}}
-                              , ExprS{rets = ["zero"], expr = BasicExprE{basic_expr = ConstE{val = FinV 0, ty = Fin p}}}
-                              , ExprS{rets = ["pi_pick"], expr = BasicExprE{basic_expr = TernaryE{branch = VarE{var = "xi"}, lhs = VarE{var = "pi"}, rhs = VarE{var = "zero"}}}}
-                              , ExprS{rets = ["new_pr"], expr = BasicExprE{basic_expr = BinOpE{bin_op = AddOp, lhs = VarE{var = "cur_pr"}, rhs = VarE{var = "pi_pick"}}}}
-                              , ExprS{rets = ["xs'"], expr = BasicExprE{basic_expr = VarE{var = "xs"}}}
-                              ]
-                        }
-                    )
+                    FunBody
+                      { param_names = ["xs"]
+                      , ret_names = ["wt"]
+                      , body_stmt =
+                          SeqS
+                            [ ExprS{rets = ["zero"], expr = BasicExprE{basic_expr = ConstE{val = FinV 0, ty = Fin w}}}
+                            , ExprS{rets = ["wt"], expr = BasicExprE{basic_expr = ConstE{val = FinV 0, ty = Fin w}}}
+                            , ForS
+                                { loop_ix = "i"
+                                , loop_ty = Fin n
+                                , loop_body =
+                                    SeqS
+                                      [ ExprS{rets = ["xi"], expr = BasicExprE{basic_expr = DynIndexE{arr_expr = VarE{var = "xs"}, ix_expr = VarE{var = "i"}}}}
+                                      , ExprS{rets = ["wi"], expr = FunCallE{fname = "Weight", args = ["i"]}}
+                                      , ExprS{rets = ["wi"], expr = BasicExprE{basic_expr = TernaryE{branch = VarE{var = "xi"}, lhs = VarE{var = "wi"}, rhs = VarE{var = "zero"}}}}
+                                      , ExprS{rets = ["wt"], expr = BasicExprE{basic_expr = BinOpE{bin_op = AddOp, lhs = VarE{var = "wt"}, rhs = VarE{var = "wi"}}}}
+                                      ]
+                                }
+                            ]
+                      }
               }
         }
     , NamedFunDef
@@ -129,47 +91,26 @@ treeGeneratorExample n w p k =
               , ret_types = [Fin p]
               , mbody =
                   Just
-                    ( FunBody
-                        { param_names = ["xs"]
-                        , ret_names = ["pr"]
-                        , body_stmt =
-                            SeqS
-                              [ ExprS{rets = ["zero"], expr = BasicExprE{basic_expr = ConstE{val = FinV 0, ty = Fin p}}}
-                              , ExprS{rets = ["xs'", "pr"], expr = LoopE{initial_args = ["xs", "zero"], loop_body_fun = "AddProfit"}}
-                              ]
-                        }
-                    )
-              }
-        }
-    , NamedFunDef
-        { fun_name = "TreeGenLoopBody"
-        , fun_def =
-            FunDef
-              { param_types = [Arr n (Fin 2), Fin w, Fin p, Fin n]
-              , ret_types = [Arr n (Fin 2), Fin w, Fin p]
-              , mbody =
-                  Just
-                    ( FunBody
-                        { param_names = ["xs", "wt", "pr", "i"]
-                        , ret_names = ["xs'", "new_wt", "new_pr"]
-                        , body_stmt =
-                            SeqS
-                              [ ExprS{rets = ["xi"], expr = BasicExprE{basic_expr = DynIndexE{arr_expr = VarE{var = "xs"}, ix_expr = VarE{var = "i"}}}}
-                              , ExprS{rets = ["y"], expr = RandomSampleE{distr_expr = BernoulliE{prob_one = 0.2}}}
-                              , ExprS{rets = ["try_pick"], expr = BasicExprE{basic_expr = BinOpE{bin_op = XorOp, lhs = VarE{var = "xi"}, rhs = VarE{var = "y"}}}}
-                              , ExprS{rets = ["wi"], expr = FunCallE{fname = "Weight", args = ["i"]}}
-                              , ExprS{rets = ["wt_picked"], expr = BasicExprE{basic_expr = BinOpE{bin_op = AddOp, lhs = VarE{var = "wt"}, rhs = VarE{var = "wi"}}}}
-                              , ExprS{rets = ["c"], expr = FunCallE{fname = "Capacity", args = []}}
-                              , ExprS{rets = ["can_fit"], expr = BasicExprE{basic_expr = BinOpE{bin_op = LEqOp, lhs = VarE{var = "wt_picked"}, rhs = VarE{var = "c"}}}}
-                              , ExprS{rets = ["should_pick"], expr = BasicExprE{basic_expr = BinOpE{bin_op = AndOp, lhs = VarE{var = "try_pick"}, rhs = VarE{var = "can_fit"}}}}
-                              , ExprS{rets = ["xs'"], expr = BasicExprE{basic_expr = UpdateArrE{arr_expr = VarE{var = "xs"}, ix_expr = VarE{var = "i"}, rhs = VarE{var = "should_pick"}}}}
-                              , ExprS{rets = ["new_wt"], expr = BasicExprE{basic_expr = TernaryE{branch = VarE{var = "should_pick"}, lhs = VarE{var = "wt_picked"}, rhs = VarE{var = "wt"}}}}
-                              , ExprS{rets = ["pi"], expr = FunCallE{fname = "Profit", args = ["i"]}}
-                              , ExprS{rets = ["pr_picked"], expr = BasicExprE{basic_expr = BinOpE{bin_op = AddOp, lhs = VarE{var = "pr"}, rhs = VarE{var = "pi"}}}}
-                              , ExprS{rets = ["new_pr"], expr = BasicExprE{basic_expr = TernaryE{branch = VarE{var = "should_pick"}, lhs = VarE{var = "pr_picked"}, rhs = VarE{var = "pr"}}}}
-                              ]
-                        }
-                    )
+                    FunBody
+                      { param_names = ["xs"]
+                      , ret_names = ["pr"]
+                      , body_stmt =
+                          SeqS
+                            [ ExprS{rets = ["zero"], expr = BasicExprE{basic_expr = ConstE{val = FinV 0, ty = Fin p}}}
+                            , ExprS{rets = ["pr"], expr = BasicExprE{basic_expr = ConstE{val = FinV 0, ty = Fin p}}}
+                            , ForS
+                                { loop_ix = "i"
+                                , loop_ty = Fin n
+                                , loop_body =
+                                    SeqS
+                                      [ ExprS{rets = ["xi"], expr = BasicExprE{basic_expr = DynIndexE{arr_expr = VarE{var = "xs"}, ix_expr = VarE{var = "i"}}}}
+                                      , ExprS{rets = ["pi"], expr = FunCallE{fname = "Profit", args = ["i"]}}
+                                      , ExprS{rets = ["pi"], expr = BasicExprE{basic_expr = TernaryE{branch = VarE{var = "xi"}, lhs = VarE{var = "pi"}, rhs = VarE{var = "zero"}}}}
+                                      , ExprS{rets = ["pr"], expr = BasicExprE{basic_expr = BinOpE{bin_op = AddOp, lhs = VarE{var = "pr"}, rhs = VarE{var = "pi"}}}}
+                                      ]
+                                }
+                            ]
+                      }
               }
         }
     , NamedFunDef
@@ -180,48 +121,36 @@ treeGeneratorExample n w p k =
               , ret_types = [Fin 2, Arr n (Fin 2)]
               , mbody =
                   Just
-                    ( FunBody
-                        { param_names = ["xs", "pr"]
-                        , ret_names = ["ok", "xs'"]
-                        , body_stmt =
-                            SeqS
-                              [ ExprS{rets = ["zero_wt"], expr = BasicExprE{basic_expr = ConstE{val = FinV 0, ty = Fin w}}}
-                              , ExprS{rets = ["zero_pr"], expr = BasicExprE{basic_expr = ConstE{val = FinV 0, ty = Fin p}}}
-                              , ExprS{rets = ["xs'", "wt'", "pr'"], expr = LoopE{initial_args = ["xs", "zero_wt", "zero_pr"], loop_body_fun = "TreeGenLoopBody"}}
-                              , ExprS{rets = ["ok"], expr = BasicExprE{basic_expr = BinOpE{bin_op = LtOp, lhs = VarE{var = "pr"}, rhs = VarE{var = "pr'"}}}}
-                              ]
-                        }
-                    )
-              }
-        }
-    , NamedFunDef
-        { fun_name = "KnapsackLoopBody"
-        , fun_def =
-            FunDef
-              { param_types = [Arr n (Fin 2), Fin k]
-              , ret_types = [Arr n (Fin 2)]
-              , mbody =
-                  Just
-                    ( FunBody
-                        { param_names = ["xs", "i"]
-                        , ret_names = ["xs_next"]
-                        , body_stmt =
-                            SeqS
-                              [ ExprS{rets = ["pr"], expr = FunCallE{fname = "TotalProfit", args = ["xs"]}}
-                              , ExprS
-                                  { rets = ["ok", "xs'"]
-                                  , expr =
-                                      PrimCallE
-                                        { prim =
-                                            Primitive
-                                              [PartialFun{pfun_name = "TreeGen", pfun_args = [Just "xs", Just "pr"]}]
-                                              (inject (QAmplify @size (Amplify{p_min = 1.0e-2 :: prec})))
-                                        }
-                                  }
-                              , ExprS{rets = ["xs_next"], expr = BasicExprE{basic_expr = TernaryE{branch = VarE{var = "ok"}, lhs = VarE{var = "xs'"}, rhs = VarE{var = "xs"}}}}
-                              ]
-                        }
-                    )
+                    FunBody
+                      { param_names = ["xs", "old_pr"]
+                      , ret_names = ["ok", "xs"]
+                      , body_stmt =
+                          SeqS
+                            [ ExprS{rets = ["wt"], expr = BasicExprE{basic_expr = ConstE{val = FinV 0, ty = Fin w}}}
+                            , ExprS{rets = ["pr"], expr = BasicExprE{basic_expr = ConstE{val = FinV 0, ty = Fin p}}}
+                            , ExprS{rets = ["c"], expr = FunCallE{fname = "Capacity", args = []}}
+                            , ForS
+                                { loop_ix = "i"
+                                , loop_ty = Fin n
+                                , loop_body =
+                                    SeqS
+                                      [ ExprS{rets = ["xi"], expr = BasicExprE{basic_expr = DynIndexE{arr_expr = VarE{var = "xs"}, ix_expr = VarE{var = "i"}}}}
+                                      , ExprS{rets = ["y"], expr = RandomSampleE{distr_expr = BernoulliE{prob_one = 0.2}}}
+                                      , ExprS{rets = ["try_pick"], expr = BasicExprE{basic_expr = BinOpE{bin_op = XorOp, lhs = VarE{var = "xi"}, rhs = VarE{var = "y"}}}}
+                                      , ExprS{rets = ["wi"], expr = FunCallE{fname = "Weight", args = ["i"]}}
+                                      , ExprS{rets = ["wt_picked"], expr = BasicExprE{basic_expr = BinOpE{bin_op = AddOp, lhs = VarE{var = "wt"}, rhs = VarE{var = "wi"}}}}
+                                      , ExprS{rets = ["can_fit"], expr = BasicExprE{basic_expr = BinOpE{bin_op = LEqOp, lhs = VarE{var = "wt_picked"}, rhs = VarE{var = "c"}}}}
+                                      , ExprS{rets = ["should_pick"], expr = BasicExprE{basic_expr = BinOpE{bin_op = AndOp, lhs = VarE{var = "try_pick"}, rhs = VarE{var = "can_fit"}}}}
+                                      , ExprS{rets = ["xs"], expr = BasicExprE{basic_expr = UpdateArrE{arr_expr = VarE{var = "xs"}, ix_expr = VarE{var = "i"}, rhs = VarE{var = "should_pick"}}}}
+                                      , ExprS{rets = ["wt"], expr = BasicExprE{basic_expr = TernaryE{branch = VarE{var = "should_pick"}, lhs = VarE{var = "wt_picked"}, rhs = VarE{var = "wt"}}}}
+                                      , ExprS{rets = ["pi"], expr = FunCallE{fname = "Profit", args = ["i"]}}
+                                      , ExprS{rets = ["pr_picked"], expr = BasicExprE{basic_expr = BinOpE{bin_op = AddOp, lhs = VarE{var = "pr"}, rhs = VarE{var = "pi"}}}}
+                                      , ExprS{rets = ["pr"], expr = BasicExprE{basic_expr = TernaryE{branch = VarE{var = "should_pick"}, lhs = VarE{var = "pr_picked"}, rhs = VarE{var = "pr"}}}}
+                                      ]
+                                }
+                            , ExprS{rets = ["ok"], expr = BasicExprE{basic_expr = BinOpE{bin_op = LtOp, lhs = VarE{var = "old_pr"}, rhs = VarE{var = "pr"}}}}
+                            ]
+                      }
               }
         }
     , NamedFunDef
@@ -232,16 +161,33 @@ treeGeneratorExample n w p k =
               , ret_types = [Arr n (Fin 2)]
               , mbody =
                   Just
-                    ( FunBody
-                        { param_names = []
-                        , ret_names = ["xs'"]
-                        , body_stmt =
-                            SeqS
-                              [ ExprS{rets = ["xs"], expr = BasicExprE{basic_expr = DefaultE{ty = Arr n (Fin 2)}}}
-                              , ExprS{rets = ["xs'"], expr = LoopE{initial_args = ["xs"], loop_body_fun = "KnapsackLoopBody"}}
-                              ]
-                        }
-                    )
+                    FunBody
+                      { param_names = []
+                      , ret_names = ["xs"]
+                      , body_stmt =
+                          SeqS
+                            [ ExprS{rets = ["xs"], expr = BasicExprE{basic_expr = DefaultE{ty = Arr n (Fin 2)}}}
+                            , ForS
+                                { loop_ix = "iter"
+                                , loop_ty = Fin k
+                                , loop_body =
+                                    SeqS
+                                      [ ExprS{rets = ["pr"], expr = FunCallE{fname = "TotalProfit", args = ["xs"]}}
+                                      , ExprS
+                                          { rets = ["ok", "xs'"]
+                                          , expr =
+                                              PrimCallE
+                                                { prim =
+                                                    Primitive
+                                                      [PartialFun{pfun_name = "TreeGen", pfun_args = [Just "xs", Just "pr"]}]
+                                                      (inject (QAmplify @size (Amplify{p_min = 1.0e-2 :: prec})))
+                                                }
+                                          }
+                                      , ExprS{rets = ["xs"], expr = BasicExprE{basic_expr = TernaryE{branch = VarE{var = "ok"}, lhs = VarE{var = "xs'"}, rhs = VarE{var = "xs"}}}}
+                                      ]
+                                }
+                            ]
+                      }
               }
         }
     ]
